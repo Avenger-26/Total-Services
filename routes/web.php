efa41566f7a79e5600c0c68ba40c022c9c87df1d
Shah Hussain <shahhussaindev@gmail.com>
2021-11-28 11:46:44 +0530

    Services Pages updated

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Livewire\HomeComponent;
 use App\Http\Livewire\ServiceCategoriesComponent;
+use App\Http\Livewire\ServicesByCategoryComponent;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 Route::get('/', HomeComponent::class)->name('home');
 Route::get('/service-categories', ServiceCategoriesComponent::class)->name('home.service_categories');
+Route::get('/{category_slug}/services', ServicesByCategoryComponent::class)->name('home.services_by_category');
